a19f10bfd581c313ed2189a4733326a16a21c568
aizeek1 <keeziallen@gmail.com>
2022-01-25 09:05:34 -0500

    Update sch

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 2022_01_24_045445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "author"
-    t.string "title"
-    t.string "genre"
-    t.float "late_fee"
-    t.integer "cdn"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
