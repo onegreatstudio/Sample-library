825016506eb6d7f6e0c6ba83376da0236822cbd3
aizeek1 <keeziallen@gmail.com>
2022-01-25 09:16:46 -0500

    Update git

--- app/javascript/session.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("turbolinks:load", function () {
-  setTimeout(function () {
-    $(".alert").alert('close');
-  }, 5000);
-})
--- app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: 'admin@onegreatstudio.com'
-  layout 'mailer'
-end
